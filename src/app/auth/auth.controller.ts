504ce052f7c84ee0b03984ce0dd5b97c2dc96c6a
Tiago Civatti Frausino <33489675+Tiagocf2@users.noreply.github.com>
2023-06-02 12:02:14 -0300

    refactor: file structure reorganization

---
+++ src/app/auth/auth.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signin')
+  @HttpCode(HttpStatus.OK)
+  signin() {}
+}
