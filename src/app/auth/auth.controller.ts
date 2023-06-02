12e10bcdbf4c460271c8f0fbc6276f91a70f1a32
Tiago Civatti Frausino <33489675+Tiagocf2@users.noreply.github.com>
2023-06-02 13:16:29 -0300

    feat: auth refactor

--- src/app/auth/auth.controller.ts
+++ src/app/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { SigninDto } from './validation/signin.validation';
+import { SigninDto } from './dto/signin.dto';
 
 @Controller('auth')
 export class AuthController {
