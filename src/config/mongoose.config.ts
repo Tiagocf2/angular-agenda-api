0705e2d65bc7a28e6d493fba956ba71f2a24ae00
Tiago Civatti Frausino <33489675+Tiagocf2@users.noreply.github.com>
2023-06-02 13:15:42 -0300

    feat: update mongoose config

--- src/config/mongoose.config.ts
+++ src/config/mongoose.config.ts
@@ -1,5 +1,21 @@
-import { MongooseModuleOptions } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {
+  MongooseModuleAsyncOptions,
+  MongooseModuleFactoryOptions,
+} from '@nestjs/mongoose';
 
-const MongooseModuleConfiguration: MongooseModuleOptions = {};
+const staticOptions: MongooseModuleFactoryOptions = {};
 
-export default MongooseModuleConfiguration;
+const MongooseConfiguration: MongooseModuleAsyncOptions = {
+  imports: [ConfigModule],
+  useFactory: async (configService: ConfigService) => {
+    console.info('Mongoose URI:', configService.get<string>('DATABASE_URI'));
+    return {
+      ...staticOptions,
+      uri: configService.get<string>('DATABASE_URI'),
+    };
+  },
+  inject: [ConfigService],
+};
+
+export default MongooseConfiguration;
