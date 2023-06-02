132d41adafe0fe183359a5aac10c06e240a08dad
Tiago Civatti Frausino <33489675+Tiagocf2@users.noreply.github.com>
2023-06-02 17:00:02 -0300

    feat: user crud

--- src/app/users/entities/user.schema.ts
+++ src/app/users/entities/user.schema.ts
@@ -1,15 +1,51 @@
 import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
 import md5 from 'md5';
+import { Document } from 'mongoose';
 
-@Schema({ autoIndex: true, toJSON: { virtuals: true } })
-export class User {
-  @Prop()
+@Schema({
+  // toJSON: { virtuals: true },
+  toObject: { virtuals: true },
+})
+export class User extends Document {
+  @Prop({ isRequired: true })
   username: string;
+
+  @Prop({ isRequired: true })
+  hashedPassword: string;
+
+  @Prop({ isRequired: true })
+  email: string;
+
+  @Prop({ isRequired: true })
+  name: string;
+
+  @Prop()
+  address: string;
+
+  set password(pwd: string) {
+    const hashed = md5(pwd);
+    this.set({ hashedPassword: hashed });
+  }
+  get password(): string {
+    return this.hashedPassword;
+  }
+
+  comparePassword(pwd: string): boolean {
+    return this.hashedPassword === md5(pwd);
+  }
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
 
-UserSchema.virtual('password').set(function (pwd: string) {
-  const hashed = md5(pwd);
-  this.set(hashed);
-});
+UserSchema.virtual('password')
+  .set(function (pwd: string) {
+    const hashed = md5(pwd);
+    this.set({ hashedPassword: hashed });
+  })
+  .get(function () {
+    return this.hashedPassword;
+  });
+
+// UserSchema.method('comparePassword', function (pwd: string): boolean {
+//   return this.hashedPassword === md5(pwd);
+// });
--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -1,20 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {}
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const newUser = new this.userModel();
+    // await newUser.save();
+    console.log('NEW', newUser);
+
+    console.log('PTA');
+    return 'oi';
+  }
 
   findAll() {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne({
+    id,
+    username,
+  }: {
+    id?: string;
+    username?: string;
+  }): Promise<User> {
+    if (!id && !username) throw new NotFoundException();
+    let user: User;
+    if (id) {
+      user = await this.userModel.findById(id).exec();
+    } else {
+      user = await this.userModel.findOne({ username }).exec();
+    }
+    if (!user) throw new NotFoundException();
+
+    return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
+  update(id: string, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
 }
