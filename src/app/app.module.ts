7d11d23ce47817d84240ca855147a95693476193
Tiago Civatti Frausino <33489675+Tiagocf2@users.noreply.github.com>
2023-06-02 12:40:54 -0300

    feat: Mongoose Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import MongooseModuleConfiguration from 'src/config/mongoose.config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(
+      process.env.DATABASE_URI,
+      MongooseModuleConfiguration,
+    ),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
