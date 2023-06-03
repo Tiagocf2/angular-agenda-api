fc01340a7cebb1ec1617b52fe248907b04bc0458
Tiago Civatti Frausino <33489675+Tiagocf2@users.noreply.github.com>
2023-06-03 20:56:00 -0300

    fix: merge conflicts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,10 @@ import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { AuthModule } from './api/auth/auth.module';
+import { UsersModule } from './api/users/users.module';
 
-import MongooseConfiguration from 'src/config/mongoose.config';
+import MongooseConfiguration from 'src/core/config/mongoose.config';
 
 @Module({
   imports: [
