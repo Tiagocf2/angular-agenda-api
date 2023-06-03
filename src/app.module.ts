16a4f6a0997eaddd806f130c92340b0e30a8fa16
Tiago Civatti Frausino <33489675+Tiagocf2@users.noreply.github.com>
2023-06-03 20:54:12 -0300

    revert: database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,17 @@ import { AppService } from './app.service';
 import { AppController } from './app.controller';
 
 import { ConfigModule } from '@nestjs/config';
-// import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { DatabaseModule } from '../core/database/database.module';
 
-// import MongooseConfiguration from 'src/config/mongoose.config';
+import MongooseConfiguration from 'src/config/mongoose.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    DatabaseModule,
-    // MongooseModule.forRootAsync(MongooseConfiguration),
+    MongooseModule.forRootAsync(MongooseConfiguration),
     AuthModule,
     UsersModule,
   ],
