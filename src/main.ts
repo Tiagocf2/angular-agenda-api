79488b70066eb65d17299d73b82f1f446508c7f2
Tiago Civatti Frausino <33489675+Tiagocf2@users.noreply.github.com>
2023-06-04 02:28:49 -0300

    feat: global "api" route prefix

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe(ValidationPipeConfiguration));
   app.enableCors();
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
