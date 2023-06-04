f76fa4265f0611796bbeb1fca96a8ca3ff7bad90
Tiago Civatti Frausino <33489675+Tiagocf2@users.noreply.github.com>
2023-06-04 01:45:22 -0300

    fix: file name

--- src/api/auth/auth.guard.ts
+++ src/api/auth/auth.guard.ts
@@ -9,6 +9,7 @@ import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 import { Observable } from 'rxjs';
+import { IS_PUBLIC_KEY } from 'src/common/decorators/is-public.decorator';
 import { Environment } from 'src/core/config/environment.config';
 
 @Injectable()
@@ -20,14 +21,13 @@ export class AuthGuard implements CanActivate {
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    // const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
-    //   context.getHandler(),
-    //   context.getClass(),
-    // ]);
-    // if (isPublic) {
-    //   // 💡 See this condition
-    //   return true;
-    // }
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
 
     const request = context.switchToHttp().getRequest();
     const token = this.getTokenFromRequest(request);
@@ -45,8 +45,10 @@ export class AuthGuard implements CanActivate {
   }
 
   private getTokenFromRequest(request: Request): string | undefined {
-    const [type, token] = request.headers.authorization?.split(' ');
-    if (type == 'Bearer') return token;
+    if (request.headers.authorization) {
+      const [type, token] = request.headers.authorization.split(' ');
+      if (type == 'Bearer') return token;
+    }
     return undefined;
   }
 }
