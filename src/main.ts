c16011a6242ba41b83ec878cf0fcc2dafb9024cf
Tiago Civatti Frausino <33489675+Tiagocf2@users.noreply.github.com>
2023-06-13 10:14:56 -0300

    feat: ssl

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import ValidationPipeConfiguration from './core/config/validation.config';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: fs.readFileSync('src/secrets/key.pem', 'utf8'),
+      cert: fs.readFileSync('src/secrets/server.crt', 'utf8'),
+    },
+  });
   app.useGlobalPipes(new ValidationPipe(ValidationPipeConfiguration));
   app.enableCors();
   app.setGlobalPrefix('api');
